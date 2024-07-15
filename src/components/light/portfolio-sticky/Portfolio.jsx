7c64f4db7ee6001f97a54b76223f0a9179964bea
Haris123456788 <waseemharis1122@gmail.com>
2024-07-15 18:54:58 +0500

    Portfolio Changes

--- src/components/light/portfolio-sticky/Portfolio.jsx
+++ src/components/light/portfolio-sticky/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Portfolio() {
   return (
@@ -7,102 +8,99 @@ function Portfolio() {
         <div className="row">
           <div className="col-lg-8 items">
             <div className="img">
-              <img src="/light/assets/imgs/works/stand/1.jpg" alt="" />
+              <img src="/light/assets/imgs/works/1/EmpowerCare.png" alt="" />
             </div>
           </div>
           <div className="col-lg-4 items">
             <div className="cont">
               <div className="sticky-item">
-                <h6 className="sub-title opacity-7 mb-10">Branding</h6>
-                <h5 className="mb-15">Aevoe Brand</h5>
+                <h6 className="sub-title opacity-7 mb-10">Development</h6>
+                <h5 className="mb-15">Empower Care</h5>
                 <p>
-                  we have been using graphic design and websites to bring brands
-                  to life.
+                Empowering You to Find Fulfilling Contract  and Per Diem Opportunities​.
+
                 </p>
-                <a href="/light/project6" className="rmore mt-30">
+                <Link to="/light/portfolio-sticky/empowercare" className="rmore mt-30">
                   <span className="sub-title">View Project</span>
                   <img
                     src="/light/assets/imgs/arrow-right.png"
                     alt=""
                     className="icon-img-20 ml-5"
                   />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
           <div className="col-lg-4 items">
             <div className="cont">
               <div className="sticky-item">
-                <h6 className="sub-title opacity-7 mb-10">Photography</h6>
-                <h5 className="mb-15">Nice guy</h5>
+                <h6 className="sub-title opacity-7 mb-10">Design & Development</h6>
+                <h5 className="mb-15">WeStackltd.com</h5>
                 <p>
-                  we have been using graphic design and websites to bring brands
-                  to life.
+                Enhancing your Business. Simplifying your Focus.
                 </p>
-                <a href="/light/project6" className="rmore mt-30">
+                <Link to="/light/portfolio-sticky/westack" className="rmore mt-30">
                   <span className="sub-title">View Project</span>
                   <img
                     src="/light/assets/imgs/arrow-right.png"
                     alt=""
                     className="icon-img-20 ml-5"
                   />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-8 items">
-            <div className="img">
-              <img src="/light/assets/imgs/works/stand/3.jpg" alt="" />
-            </div>
-          </div>
-          <div className="col-lg-8 items">
-            <div className="img">
-              <img src="/light/assets/imgs/works/stand/5.jpg" alt="" />
-            </div>
-          </div>
-          <div className="col-lg-4 items">
-            <div className="cont">
-              <div className="sticky-item">
-                <h6 className="sub-title opacity-7 mb-10">Branding</h6>
-                <h5 className="mb-15">Digital Design</h5>
-                <p>
-                  we have been using graphic design and websites to bring brands
-                  to life.
-                </p>
-                <a href="/light/project6" className="rmore mt-30">
-                  <span className="sub-title">View Project</span>
-                  <img
-                    src="/light/assets/imgs/arrow-right.png"
-                    alt=""
-                    className="icon-img-20 ml-5"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 items">
-            <div className="cont">
-              <div className="sticky-item">
-                <h6 className="sub-title opacity-7 mb-10">Branding</h6>
-                <h5 className="mb-15">Digital Design</h5>
-                <p>
-                  we have been using graphic design and websites to bring brands
-                  to life.
-                </p>
-                <a href="/light/project6" className="rmore mt-30">
-                  <span className="sub-title">View Project</span>
-                  <img
-                    src="/light/assets/imgs/arrow-right.png"
-                    alt=""
-                    className="icon-img-20 ml-5"
-                  />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
           <div className="col-lg-8 items">
             <div className="img">
-              <img src="/light/assets/imgs/works/stand/7.jpg" alt="" />
+              <img src="/light/assets/imgs/works/1/WeStack.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-8 items">
+            <div className="img">
+              <img src="/light/assets/imgs/works/1/UnitedEstates.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-4 items">
+            <div className="cont">
+              <div className="sticky-item">
+                <h6 className="sub-title opacity-7 mb-10">Development</h6>
+                <h5 className="mb-15">United Estates</h5>
+                <p>
+                We have made out quality development an hallmark by incorporating the latest in contemporary architecture to suit your tastes and budget.
+                </p>
+                <Link to="/light/portfolio-sticky/unitedestates" className="rmore mt-30">
+                  <span className="sub-title">View Project</span>
+                  <img
+                    src="/light/assets/imgs/arrow-right.png"
+                    alt=""
+                    className="icon-img-20 ml-5"
+                  />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 items">
+            <div className="cont">
+              <div className="sticky-item">
+                <h6 className="sub-title opacity-7 mb-10">Development</h6>
+                <h5 className="mb-15">Bare Beauty</h5>
+                <p>
+                Embrace Your Natural Glow with Bare Beauty.
+                </p>
+                <Link to="/light/portfolio-sticky/barebeauty" className="rmore mt-30">
+                  <span className="sub-title">View Project</span>
+                  <img
+                    src="/light/assets/imgs/arrow-right.png"
+                    alt=""
+                    className="icon-img-20 ml-5"
+                  />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-8 items">
+            <div className="img">
+              <img src="/light/assets/imgs/works/1/BareBeauty.png" alt="" />
             </div>
           </div>
         </div>
