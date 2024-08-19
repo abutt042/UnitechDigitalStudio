01a7cf1b4ce6ecd42d92e0e5e8c34a6c14ebd665
Abdullah <abutt042@gmail.com>
2024-08-19 18:20:42 +0500

    Contact Form Update

--- src/components/light/modern-startup/Marquee.jsx
+++ src/components/light/modern-startup/Marquee.jsx
@@ -16,47 +16,27 @@ const imgStyle = {
 return (
   <div className='container' style={containerStyle}>
     <img
-<<<<<<< HEAD
-      src="/light/assets/imgs/clients/clutch-3.png"
-=======
       src="https://unitechdigitalstudio.com/wp-content/uploads/2024/05/clutch-3.png"
->>>>>>> ac0d212597fb7b06f081de15ea6ff44131030444
       alt="Image 1"
       style={imgStyle}
     />
     <img
-<<<<<<< HEAD
-      src="/light/assets/imgs/clients/google.png"
-=======
       src="https://unitechdigitalstudio.com/wp-content/uploads/2024/05/google.png"
->>>>>>> ac0d212597fb7b06f081de15ea6ff44131030444
       alt="Image 2"
       style={imgStyle}
     />
     <img
-<<<<<<< HEAD
-      src="/light/assets/imgs/clients/shopify.png"
-=======
       src="https://unitechdigitalstudio.com/wp-content/uploads/2024/05/trustpilot.png"
->>>>>>> ac0d212597fb7b06f081de15ea6ff44131030444
       alt="Image 3"
       style={imgStyle}
     />
     <img
-<<<<<<< HEAD
-      src="/light/assets/imgs/clients/themanifest.png"
-=======
       src="https://unitechdigitalstudio.com/wp-content/uploads/2024/05/themanifest.png"
->>>>>>> ac0d212597fb7b06f081de15ea6ff44131030444
       alt="Image 4"
       style={imgStyle}
     />
     <img
-<<<<<<< HEAD
-      src="/light/assets/imgs/clients/trustpilot.png"
-=======
       src="https://unitechdigitalstudio.com/wp-content/uploads/2024/05/shopify-1024x171.png"
->>>>>>> ac0d212597fb7b06f081de15ea6ff44131030444
       alt="Image 5"
       style={imgStyle}
     />
