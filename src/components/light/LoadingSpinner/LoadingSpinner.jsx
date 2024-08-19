df5408f5cfc6ca18d9253096b95e6f2f203fe5aa
Abdullah <abutt042@gmail.com>
2024-08-19 19:27:59 +0500

    loader change

--- src/components/light/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/light/LoadingSpinner/LoadingSpinner.jsx
@@ -1,12 +1,16 @@
+import React from 'react';
+import './LoadingSpinner.css';
 
- import React from 'react';
- import './LoadingSpinner.css';
- 
- const LoadingSpinner = () => (
-   <div className="spinner-container">
-     <div className="spinner"></div>
-   </div>
- );
- 
- export default LoadingSpinner;
- 
+const LoadingSpinner = () => (
+  <div className="spinner-container">
+    <div className="bouncing-dots">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      
+    </div>
+  </div>
+);
+
+export default LoadingSpinner;
